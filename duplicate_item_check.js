f44566bdec7b106587cadcec8000797db36d2ab4
YourHat <65801916+YourHat@users.noreply.github.com>
2025-03-09 20:15:42 -0400

    Create duplicate_item_check.js

---
+++ duplicate_item_check.js
@@ -0,0 +1,25 @@
+(() => {
+  'use strict';
+  kintone.events.on('app.record.create.submit', (event) => {
+  var record = event.record;
+  var itemname = record.item_name.value;
+  var seller = record.seller.value;
+  var query = 'item_name="'+ itemname +'" and seller="'+seller+'"';
+  var params = {app:6,query};
+  return kintone.api('/k/v1/records','GET', params).then((resp)=>{
+    if(!resp.records.length){
+      alert("New Contact Added!");
+    }else{
+      record.item_namem.error = 'duplicate item found';
+      record.seller.error = 'duplicate item found';
+
+    }
+    return event;
+  }).catch((resp)=>{
+    alert("error occured");
+  }
+  
+)
+    
+  });
+})();
